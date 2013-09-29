4c292869631e500e03c69573c685c220a77cc51c
tokiwoousaka <its.out.of.tune.this.my.music@gmail.com>
2013-09-30 07:05:32 +0900

    Added ararePlay function

--- src/Sound/ArareSynth.hs
+++ src/Sound/ArareSynth.hs
@@ -1,6 +1,13 @@
 module Sound.ArareSynth where
+import Control.DeepSeq
 import Sound.ArareSynth.Oscillator
 import Sound.ArareSynth.Types
 import Sound.Sarasvati
 
+data ArareConfig = ArareConfig
 
+arareDefault :: ArareConfig
+arareDefault = ArareConfig
+
+ararePlay :: Channel c h => ArareConfig -> [c] -> IO (Either Error ())
+ararePlay _ w = sarasvatiOutput defaultConfig $!! w
